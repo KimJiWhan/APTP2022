1593cf7e3a1d5e3ad55f151071a46aa2ca53d297
KimJW <jiwhan1015@yonsei.ac.kr>
2022-04-30 00:02:08 +0900

    22.04.30. AI Init

---
+++ snake_AI.py
@@ -0,0 +1,8 @@
+from snake_ForAI import snakeGameAI as snake
+
+class snake_AI:
+    def __init__(self):
+        self.a = 2
+
+if __name__ == "__main__":
+    snake_AI = snake_AI()
--- snake_ForAI.py
+++ snake_ForAI.py
@@ -3,43 +3,56 @@ import random
 from enum import Enum
 from collections import namedtuple
 
+pygame.init()
+
+# Fonts
+font = pygame.font.Font('arial.ttf', 20)
+
 # Moving Direction
 class Dir(Enum):
     UP = 0
+    RIGHT = 1
     DOWN = 2
     LEFT = 3
-    RIGHT = 1
+
 
 # Coordination of point
 Cord = namedtuple('Cord', ['x', 'y'])
-pygame.init()
-# Fonts
-font = pygame.font.Font('arial.ttf', 20)
+
 # RGBs
 WHITE = (255, 255, 255)
 GREY = (192, 192, 192)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-BLUE1 = (0, 0, 255)
-BLUE2 = (92, 92, 255)
 
 # Constants
 oneBlockSize = 20
 headSpeed = 20
 
-class snakeGame:
-    def __init__(self, w=960, h=960):
+class snakeGameAI:
+    def __init__(self, w = 720, h = 720):
+        # CONSTANTS (will not be reset)
         self.width = w
         self.height = h
         self.display = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('Asterisk->SnakeGame')
         self.clock = pygame.time.Clock()
-        # initialize the snake (head at center, two blocks at tail)
-        self.dir = Dir.RIGHT
 
-        self.head = Cord((self.width)/2, (self.height)/2)
-        self.snake = [self.head, Cord(self.head.x - oneBlockSize, self.head.y), Cord(self.head.x - (2 * oneBlockSize), self.head.y)]
-        self.item = Cord(0, 0)
+        self.dir = Dir.RIGHT
+        self.head = Cord((self.width) / 2, (self.height) / 2)
+        self.snake = [self.head, Cord(self.head.x - oneBlockSize, self.head.y),
+                      Cord(self.head.x - (2 * oneBlockSize), self.head.y)]
+        self.item = Cord(random.randint(0, (self.width - oneBlockSize) // oneBlockSize) * oneBlockSize,
+                         random.randint(0, (self.height - oneBlockSize) // oneBlockSize) * oneBlockSize)
+        self.tail = 0
+
+    def reset(self):
+        self.dir = Dir.RIGHT
+        self.head = Cord((self.width) / 2, (self.height) / 2)
+        self.snake = [self.head, Cord(self.head.x - oneBlockSize, self.head.y),
+                      Cord(self.head.x - (2 * oneBlockSize), self.head.y)]
+        self.item = Cord(random.randint(0, (self.width - oneBlockSize) // oneBlockSize) * oneBlockSize,
+                         random.randint(0, (self.height - oneBlockSize) // oneBlockSize) * oneBlockSize)
         self.tail = 0
 
     def _locateItem(self):
@@ -62,7 +75,7 @@ class snakeGame:
         elif direction == Dir.DOWN:
             y += oneBlockSize
         elif direction == Dir.LEFT:
-            y -= oneBlockSize
+            x -= oneBlockSize
         self.head = Cord(x, y)
 
     def _collision(self):
@@ -80,29 +93,17 @@ class snakeGame:
         self.display.fill(BLACK)
 
         for cord in self.snake:
-            pygame.draw.rect(self.display, BLUE1, pygame.Rect(cord.x, cord.y, oneBlockSize, oneBlockSize))
-            pygame.draw.rect(self.display, BLUE2, pygame.Rect(cord.x+4, cord.y+4, 12, 12))
+            pygame.draw.rect(self.display, WHITE, pygame.Rect(cord.x, cord.y, oneBlockSize, oneBlockSize))
+            pygame.draw.rect(self.display, GREY, pygame.Rect(cord.x+4, cord.y+4, 12, 12))
         pygame.draw.rect(self.display, RED, pygame.Rect(self.item.x, self.item.y, oneBlockSize, oneBlockSize))
 
-        text = font.render("Score: " + str(self.tail), True, WHITE)
-        self.display.blit(text, [0, 0])
+        text0 = font.render("Score: " + str(self.tail), True, WHITE)
+        self.display.blit(text0, [0, 0])
         pygame.display.flip()
 
-    def play(self):
-        # Get Input
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    self.dir = Dir.UP
-                elif event.key == pygame.K_DOWN:
-                    self.dir = Dir.DOWN
-                elif event.key == pygame.K_LEFT:
-                    self.dir = Dir.LEFT
-                elif event.key == pygame.K_RIGHT:
-                    self.dir = Dir.RIGHT
-            elif event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+    def play(self, execute):
+        # Set Input
+
 
         # Move the snake by head
         self._move(self.dir)
@@ -114,25 +115,15 @@ class snakeGame:
             game_over = True
             return game_over, self.tail
 
-        # place new food if met
+        # Place new food if the head encounters the item
         if self.head == self.item:
             self.tail += 1
             self._locateItem()
         else:
             self.snake.pop()
 
-        # update ui
+        # update UI
         self._ui()
         self.clock.tick(headSpeed)
 
         return game_over, self.tail
-
-if __name__ == "__main__":
-    game = snakeGame()
-
-    while True:
-        game_over, score = game.play()
-        if game_over == True:
-            break
-    print("Final Score: ", score)
-    pygame.quit()
